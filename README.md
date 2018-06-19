9638a6e828904923f12c2cada72e0b55fe306df4
yzh <yuanzhihang1@126.com>
2018-06-19 14:09:44 +0800

    detect face OK

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # MTCNN_API
-Convenient API of MTCNN face or face landmark detection
+Convenient API of MTCNN face or face landmark detection.
+
+Joint Face Detection and Alignment using Multi-task Cascaded Convolutional Neural Networks
+
+Tensorflow implementation of the face detection / alignment algorithm found at
+https://github.com/kpzhang93/MTCNN_face_detection_alignment
+
+I will going to support more framework such as Caffe, Pytorch and Mxnet.
+
+- [x] Tensorflow
+- [] Caffe
+- [] Pytorch
+- [] Mxnet
+
+# Requirement
+* tensorflow
+* python2.x or python3.x
+* cv2
+
+# API
+
+This is a python package and you should `import MTCNN_API`.
+
+### detect_cv2_ims
+MTCNN_API.detect_cv2_ims(images,minsize=20,threshold=(0.6, 0.7, 0.7),scale_factor=0.709,gpu_fraction=0.5)
+
+* images: a list contains cv2 opened images
+* minsize: minimum size of face
+* threshold: three steps's threshold
+* scale_factor: scale factor
+* gpu_fraction: tensorflow gpu_fraction
+
+return: (boxes, landmarks)
+* boxes is a list contains ndarray with shape (n_faces_in_pic, 5), 5 number represent for (x1,y1,x2,y2,score).
+* landmarks is a list contains ndarray with shape(n_faces_in_pic, 10),10 landmark number represent for
+        (leyex,reyex,nosex,lmouthx,rmouthx,leyey,reyey,nosey,lmouthy,rmouthy) l:Left, r:Right
+
+# Example
+
+    import cv2
+    import MTCNN_API
+
+    images = []
+    for file_name in ['test1.jpg', 'test2.png', 'test3.png']:
+        im = cv2.imread(file_name)
+        images.append(im)
+
+    boxes, landmarks = MTCNN_API.detect_cv2_ims(images)
---
+++ __init__.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
+import os
+from . import detect_face
+
+_MTCNN=[]
+
+def get_net(gpu_fraction=0.5):
+    global _MTCNN
+    if len(_MTCNN)!=3:
+        with tf.Graph().as_default():
+            gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
+            sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+            with sess.as_default():
+                pnet, rnet, onet = detect_face.create_mtcnn(sess, os.path.join(os.path.split(__file__)[0],'weights'))
+
+            _MTCNN+=[pnet,rnet,onet]
+    else:
+        pnet, rnet, onet=_MTCNN
+    return pnet,rnet,onet
+
+
+def detect_cv2_ims(images,minsize=20,threshold=(0.6, 0.7, 0.7),scale_factor=0.709,gpu_fraction=0.5):
+    """
+    :param images: a list contains cv2 opened images
+    :param minsize: minimum size of face
+    :param threshold: three steps's threshold
+    :param scale_factor: scale factor
+    :param gpu_fraction: tensorflow gpu_fraction
+    :return: (boxes, landmarks),
+    boxes is a list contains ndarray with shape (n_faces_in_pic, 5), 5 number represent for (x1,y1,x2,y2,score).
+    landmarks is a list contains ndarray with shape(n_faces_in_pic, 10),10 landmark number represent for
+        (leyex,reyex,nosex,lmouthx,rmouthx,leyey,reyey,nosey,lmouthy,rmouthy) l:Left, r:Right
+    """
+    pnet, rnet, onet=get_net(gpu_fraction)
+    if images is not list:
+        images=[images]
+    boxes=[]
+    landmarks=[]
+    for bgr in images:
+        rgb = bgr[..., ::-1]
+        box, landmark = detect_face.detect_face(rgb, minsize, pnet, rnet, onet, threshold, scale_factor)
+        boxes.append(box)
+        landmarks.append(landmark)
+    return boxes,landmarks
